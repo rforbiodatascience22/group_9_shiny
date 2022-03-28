48d65284ba1c040b9abe72b1a3cc8c4a8fa387df
synnespjelkavik <s174526@student.dtu.dk>
2022-03-28 08:04:53 +0000

    Changes in dev/01_start.R and README file (knitted).

--- dev/01_start.R
+++ dev/01_start.R
@@ -19,12 +19,12 @@
 ## to change the name in the app_sys() function in app_config.R /!\
 ##
 golem::fill_desc(
-  pkg_name = "myshinyapp", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  pkg_name = "centraldogapp", # The Name of the package containing the App
+  pkg_title = "Central dogma app", # The Title of the package containing the App
+  pkg_description = "Making app. Change this later.", # The Description of the package containing the App
+  author_first_name = "Synne", # Your First Name
+  author_last_name = "Spjelkavik", # Your Last Name
+  author_email = "s174526@student.dtu.dk", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional)
 )
 
@@ -33,11 +33,11 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license("group9") # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_code_of_conduct(contact = "group9")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
